2bf8f63c30c4d0516ea441ede1b568bcf0dde725
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-04-26 16:32:46 +0200

    error handling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,8 @@ import {useEffect, useState} from 'react'
 
 function App() {
     const [country, setCountry] = useState([])
-    console.log(country)
+    const [error, setError] = useState(false)
+    console.log("error",error)
     useEffect(() => {
         fetch(`https://restcountries.eu/rest/v2/all`)
             .then((response) => response.json())
@@ -11,10 +12,16 @@ function App() {
                 const selectedCountry = Math.floor(Math.random() * json.length)
                 const dataCountry = json[selectedCountry]
                 setCountry(dataCountry)
+                setError(false)
+            })
+            .catch(()=> {
+                setError(true)
             })
     }, [])
 
-    return <div className="App"></div>
+    return <div className="App">
+        {error && <p>Error during data loading!</p>}
+    </div>
 }
 
 export default App
