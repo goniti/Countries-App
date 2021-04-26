748691aee4f16bb9fee8dd11968bf53fe86baee5
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-04-26 18:19:22 +0200

    setting up the loading

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,10 @@ import {useEffect, useState} from 'react'
 function App() {
     const [country, setCountry] = useState([])
     const [error, setError] = useState(false)
-    console.log("error",error)
+    const [loading, setLoading] = useState(true)
+    console.log("loading",loading)
+    console.log(country.length)
+
     useEffect(() => {
         fetch(`https://restcountries.eu/rest/v2/all`)
             .then((response) => response.json())
@@ -17,10 +20,14 @@ function App() {
             .catch(()=> {
                 setError(true)
             })
+            .finally(() => {
+                setLoading(true)
+            })
     }, [])
 
     return <div className="App">
-        {error && <p>Error during data loading!</p>}
+        {error && <p>Error</p>}
+        {loading && country.length === 0 && <p>Loading...</p>}
     </div>
 }
 
