f554e9af01b51c799eaa97415be6072b02968ee3
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-04-26 12:52:33 +0200

    init project use CRA

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Countries-App
+# Countries-Index
+
+
+Creation of a user interface using React.
+It allows to display simple information about a country.
+
+For this, we will use a REST API
+called "restcountries".
---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function Index() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Countries APP
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Index;
